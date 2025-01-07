6dfe62699abb37fb77fd53956cc820bc68d583dd
riadjoseph <riadjoseph@icloud.com>
2025-01-07 01:23:15 +0100

    modify 404 behaviour

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,21 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        404: path.resolve(__dirname, '404.html')
+      }
+    }
+  },
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
