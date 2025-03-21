08eb9fb36bfe7681170387b8ffaf61ddca786fa9
riadjoseph <riadjoseph@icloud.com>
2025-03-21 22:08:31 +0100

    Refactor MarkdownDisplay component to simplify rendering and enhance styling

--- MarkdownDisplay.tsx
+++ MarkdownDisplay.tsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
 
 interface MarkdownDisplayProps {
   markdown: string;
@@ -7,11 +7,9 @@ interface MarkdownDisplayProps {
 
 const MarkdownDisplay = ({ markdown }: MarkdownDisplayProps) => {
   return (
-    <ReactMarkdown
-      children={markdown}
-      remarkPlugins={[remarkGfm]}
-      className="prose prose-sm md:prose-base prose-headings:font-bold prose-strong:text-black dark:prose-strong:text-white" // Add the prose class here
-    />
+    <div className="prose dark:prose-invert max-w-none">
+      <ReactMarkdown>{markdown}</ReactMarkdown>
+    </div>
   );
 };
 
