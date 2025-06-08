6f476aa1f5a3e518a305ff3fe56e7fe447ba5d20
riadjoseph <riadjoseph@icloud.com>
2025-06-08 18:02:16 +0200

    add posthog

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { initGA, usePageTracking } from '@/utils/analytics';
+import posthog from 'posthog-js'
 
 const MEASUREMENT_ID = 'G-4S7FY23V18'; // Replace with your GA4 measurement ID
 
@@ -12,5 +13,12 @@ const Analytics = () => {
 
   return null;
 };
+
+posthog.init('phc_soH2FL1IO9Vzof8zMGv1FHyz4eIFdyuiKcMxUC9oerO',
+    {
+        api_host: 'https://eu.i.posthog.com',
+        person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+    }
+)
 
 export default Analytics; 
